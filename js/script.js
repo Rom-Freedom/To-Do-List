83953d9d77be40e2b15a3c69f3997007ce0e17f8
Rom-Freedom <romeo.dark@mail.ru>
2024-02-15 15:45:50 +0300

    delete by ctrl + right click

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ if (localStorage.getItem('todo')) {
 }
 
 addButton.addEventListener('click', function() {
+    if (!addMessage.value) return;
     //Object for our last message
     let newTodo = {
         todo: addMessage.value,
@@ -25,18 +26,20 @@ addButton.addEventListener('click', function() {
 todoList.push(newTodo);  
 displayMessages() //to call every time when the add button is pressed
 localStorage.setItem('todo', JSON.stringify(todoList)); //to store data as a String 
+addMessage.value = '';
 });
 
 //Function to display our messages(to list every todoList object and display it)
 function displayMessages() {
     let displayMessage = '';
+    if (todoList.length === 0) todo.innerHTML = '';
     //forEach accepts three parameters(object, index, array. We use just two parameters)
     todoList.forEach(function(item, index) {
         //create a list with user input
         displayMessage += `
         <li>
           <input type='checkbox' id='item_${index}' ${item.checked ? 'checked' : ''}> 
-          <label for='item_${index}'>${item.todo}</label>
+          <label for='item_${index}' class="${item.important ? 'important' : ''}">${item.todo}</label>
         </li>
         `;
         //Add into todo a message from the user with InnerHTML property
@@ -56,6 +59,23 @@ todo.addEventListener('change', function(event) {
     todoList.forEach(function(item){
         if (item.todo === valueLabel) {
             item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
+
+//Process a Context Menu
+todo.addEventListener('contextmenu', function(event) {
+    //Call off a standart browser behavior
+    event.preventDefault();
+    todoList.forEach(function(item, index) {
+        if (item.todo === event.target.innerHTML) {
+            if (event.ctrlKey) {
+                todoList.splice(index, 1);
+            } else {
+                item.important = !item.important;
+            }
+            displayMessages();
             localStorage.setItem('todo', JSON.stringify(todoList));
         }
     });
